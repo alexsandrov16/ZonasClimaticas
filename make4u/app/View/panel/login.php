241944c3bf70981cc37a79efade07619d26924f4
alexsandrov16 <alejandro920131@gmail.com>
2022-09-18 21:28:37 -0400

    asd

--- make4u/app/View/panel/login.php
+++ make4u/app/View/panel/login.php
@@ -9,7 +9,7 @@ use Make4U\Make4U;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Change this</title>
 
     <?= $theme->css('bootstrap.min.css',true),$theme->favicon()?>
   
